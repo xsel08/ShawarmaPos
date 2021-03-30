888d9a14b4737518c020c5624121fad46aed3d62
bradgarabiles <81624004+bradgarabiles@users.noreply.github.com>
2021-03-30 20:55:25 +0800

    Agbo

--- WindowsFormsApp1/Agbo4everBentetres.cs
+++ WindowsFormsApp1/Agbo4everBentetres.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Sample : Form
+    public partial class Agbo4everBentetres : Form
     {
-        public Sample()
+        public Agbo4everBentetres()
         {
             InitializeComponent();
         }
-
     }
 }
